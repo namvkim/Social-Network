1ebe048dfc1e8f12ca933eb9ed6eef2fc999cdd9
hanguyen2911 <nguyenthithuha2911ntth@gmail.com>
2021-01-26 17:24:10 +0700

    email

--- loginIn.js
+++ loginIn.js
@@ -15,8 +15,8 @@ function validate() {
     alert("Vui lòng điền thông tin chính xác!")
     return true;
 }
-var aCong = email.indexOf("@");
-var dauCham = email.lastIndexOf(".");
+var aCong = e.indexOf("@");
+var dauCham = e.lastIndexOf(".");
 if ((aCong < 1) || (dauCham < aCong + 2) || (dauCham + 2 > email.length)) {
     alert("Email bạn điền không chính xác");
     return false;
@@ -24,18 +24,3 @@ if ((aCong < 1) || (dauCham < aCong + 2) || (dauCham + 2 > email.length)) {
 
 
 
-
-// function sendEmail() {
-//     Email.send({
-//         Host: "smtp.gmail.com",
-//         Username: "<sender’s email address>",
-//         Password: "<email password>",
-//         To: '<recipient’s email address>',
-//         From: "<sender’s email address>",
-//         Subject: "<email subject>",
-//         Body: "<email body>",
-//     })
-//         .then(function (message) {
-//             alert("mail sent successfully")
-//         });
-// }
--- signUp.js
+++ signUp.js
@@ -1,43 +1,43 @@
-function validate() {
-    var firstName = document.getElementById("first_name").value;
-    var lastName = document.getElementById("last_name").value;
-    var email = document.forms["myForm"]["email"].value;
-    email = document.getElementById("email").value;
-    var pass = document.getElementById("pass").value;
-    var cPass = document.getElementById("confirmPass").value;
-    var date = document.getElementById("date").value;
-    if (firstName == "") {
-      alert("Vui lòng điền tên!");
-      return false;
-    }
-    if (lastName == "") {
-      alert("Vui lòng điền họ!");
-      return false;
-    }
-    if (email == "") {
-      alert("Vui lòng điền số điện thoại hoặc Email!");
-      return false;
-    }
-    if (pass == "") {
-      alert("Vui lòng điền mật khẩu!");
-      return false;
-    }
+// function validate() {
+//     var firstName = document.getElementById("first_name").value;
+//     var lastName = document.getElementById("last_name").value;
+//     var email = document.forms["myForm"]["email"].value;
+//     email = document.getElementById("email").value;
+//     var pass = document.getElementById("pass").value;
+//     var cPass = document.getElementById("confirmPass").value;
+//     var date = document.getElementById("date").value;
+//     if (firstName == "") {
+//       alert("Vui lòng điền tên!");
+//       return false;
+//     }
+//     if (lastName == "") {
+//       alert("Vui lòng điền họ!");
+//       return false;
+//     }
+//     if (email == "") {
+//       alert("Vui lòng điền số điện thoại hoặc Email!");
+//       return false;
+//     }
+//     if (pass == "") {
+//       alert("Vui lòng điền mật khẩu!");
+//       return false;
+//     }
   
-    if (cPass == "") {
-      alert("Vui lòng xác nhận mật khẩu!");
-      return false;
-    }
-    if (date == "") {
-      alert("Vui lòng điền ngày sinh!");
-      return false;
-    }
-    alert("Vui lòng điền thông tin chính xác!")
-    return true;
-    }
-    var aCong=email.indexOf("@");
-    var dauCham = email.lastIndexOf(".");
-    if ((aCong<1) || (dauCham<aCong+2) || (dauCham+2>email.length)) {
-    alert("Email bạn điền không chính xác");
-    return false;
-    }
+//     if (cPass == "") {
+//       alert("Vui lòng xác nhận mật khẩu!");
+//       return false;
+//     }
+//     if (date == "") {
+//       alert("Vui lòng điền ngày sinh!");
+//       return false;
+//     }
+//     alert("Vui lòng điền thông tin chính xác!")
+//     return true;
+//     }
+//     var aCong=email.indexOf("@");
+//     var dauCham = email.lastIndexOf(".");
+//     if ((aCong<1) || (dauCham<aCong+2) || (dauCham+2>email.length)) {
+//     alert("Email bạn điền không chính xác");
+//     return false;
+//     }
 
