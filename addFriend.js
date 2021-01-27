3d4cf8fb486d4c165142e16ed3d04cc6ecb6d7f0
Ho Thi Da Ni <ni.ho22@pn.com.vn>
2021-01-27 19:15:43 +0700

    Add

---
+++ addFriend.js
@@ -0,0 +1,9 @@
+function myFFunction1() {
+	document.getElementById("demo1").innerHTML = "Đã bạn bè";
+   }
+  
+   function myFFunction2() {
+	  document.getElementById("demo2").innerHTML = "Đã gửi lời mời";
+	 }
+	
+  
