e3431d2464309ec6a9dab4aaf0f6e3b000b954f8
Ho Thi Da Ni <ni.ho22@pn.com.vn>
2021-01-28 19:31:02 +0700

    Add API

--- addFriend.js
+++ addFriend.js
@@ -44,111 +44,109 @@
             callAPI(`user/${id_user}/addfriend`, "GET", null).then(res => {
                 addfriend = res.data;
                 console.log(addfriend);
+                add();
             });
         });
     }
 
-    // get_user();
+    get_user();
 
 
 
-    function add() {
-        setTimeout(()=> document.getElementById("hienthi").innerHTML += `
-            <div class="m-4">
-                <a href="#">
-                    <div class="mb-3 a">
-                        <div style=" width: 100px;">
-                            <img class="rounded-circle"
-                                src="https://scr.vn/wp-content/uploads/2020/08/%E1%BA%A2nh-trai-cute-d%E1%BB%85-th%C6%B0%C6%A1ng.jpg"
-                                style="width:65%">
-                        </div>
-                        <div class=" text-bottom">
-                            <span class="">
-                                 <p style="margin-bottom: 0px;"><b>Join Tee</b></p>
-                                <p class="text-right" style="margin-bottom: 0px;">5 giờ</p>
-                            </span>
+    // function add() {
+    //     setTimeout(()=> document.getElementById("hienthi").innerHTML += `
+    //         <div class="m-4">
+    //             <a href="#">
+    //                 <div class="mb-3 a">
+    //                     <div style=" width: 100px;">
+    //                         <img class="rounded-circle"
+    //                             src="https://scr.vn/wp-content/uploads/2020/08/%E1%BA%A2nh-trai-cute-d%E1%BB%85-th%C6%B0%C6%A1ng.jpg"
+    //                             style="width:65%">
+    //                     </div>
+    //                     <div class=" text-bottom">
+    //                         <span class="">
+    //                              <p style="margin-bottom: 0px;"><b>Join Tee</b></p>
+    //                             <p class="text-right" style="margin-bottom: 0px;">5 giờ</p>
+    //                         </span>
 
 
-                            <a type="button" id="demo1" onclick="myFFunction1()"
-                                class="btn btn-primary btn-sm d-inline-block"> Xác nhận</a>
-                            <a type="button" class="btn btn-secondary btn-sm d-inline-block"
-                                style="margin-top:0px;">Xóa</a>
-                        </div>
-                    </div>
-                </a>
-            </div>
-        `,2000)
+    //                         <a type="button" id="demo1" onclick="myFFunction1()"
+    //                             class="btn btn-primary btn-sm d-inline-block"> Xác nhận</a>
+    //                         <a type="button" class="btn btn-secondary btn-sm d-inline-block"
+    //                             style="margin-top:0px;">Xóa</a>
+    //                     </div>
+    //                 </div>
+    //             </a>
+    //         </div>
+    //     `,2000)
         
     
-    }
+    // }
 
-    add();
+    // add();
 
 
 
      //lấy data từ api
 
-    //  function add() {
-    //     for( var i in addfriend){
-    //     setTimeout(()=> document.getElementById("hienthi").innerHTML += `
+     function add() {
+        for( var i in addfriend){
+        document.getElementById("hienthi").innerHTML += `
+        <div class="m-4">
+                     <a href="#">
+                     <div class="mb-3 a">
+                             <div style=" width: 100px;">
+                                 <img class="rounded-circle"
+                                   src="${user[addfriend[i].id_add-1].avatar}"
+                                    style="width:65%">
+                             </div>
+                             <div class=" text-bottom">
+                                 <span class="">
+                                      <p style="margin-bottom: 0px;"><b>${user[addfriend[i].id_add-1].name}</b></p>
+                                     <p class="text-right" style="margin-bottom: 0px;">5 giờ</p>
+                                 </span>
+                                 <a type="button" id="demo1" onclick="myFFunction1()"
+                                     class="btn btn-primary btn-sm d-inline-block"> Xác nhận</a>
+                                 <a type="button" class="btn btn-secondary btn-sm d-inline-block"
+                                     style="margin-top:0px;">Xóa</a>
+                             </div>
+                         </div>
+                     </a>
+                 </div>
+        `
+        }
+    }
+
+  
+
+    // function add1() {
+    //     setTimeout(()=> document.getElementById("hienthi1").innerHTML += `
     //     <div class="m-4">
-    //                  <a href="#">
-    //                  <div class="mb-3 a">
-    //                          <div style=" width: 100px;">
-    //                              <img class="rounded-circle"
-    //                                src="${user[addfriend[i].id_add-1].avatar}"
-    //                                 style="width:65%">
-    //                          </div>
-    //                          <div class=" text-bottom">
-    //                              <span class="">
-    //                                   <p style="margin-bottom: 0px;"><b>${user[addfriend[i].id_add-1].name}</b></p>
-    //                                  <p class="text-right" style="margin-bottom: 0px;">5 giờ</p>
-    //                              </span>
-    
-    
-    //                              <a type="button" id="demo1" onclick="myFFunction1()"
-    //                                  class="btn btn-primary btn-sm d-inline-block"> Xác nhận</a>
-    //                              <a type="button" class="btn btn-secondary btn-sm d-inline-block"
-    //                                  style="margin-top:0px;">Xóa</a>
-    //                          </div>
-    //                      </div>
-    //                  </a>
-    //              </div>
+    //         <a href="#">
+    //             <div class="mb-3 a">
+    //                 <div style=" width: 100px;">
+    //                     <img class="rounded-circle"
+    //                         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbKoihkRWHsOFdnwoxoWhkuhb4rC2ViOiy6g&amp;usqp=CAU"
+    //                         style="width:65%">
+    //                 </div>
+    //                 <div class=" text-bottom">
+    //                     <span class="">
+    //                         <p style="margin-bottom: 0px;"><b>Nguyên </b></p>
+    //                     </span>
+
+
+    //                     <a type="button" id="demo2" onclick="myFFunction2()"
+    //                         class="btn btn-primary btn-sm d-inline-block"> Thêm bạn bè</a>
+    //                 </div>
+    //             </div>
+    //         </a>
+    //     </div>
     //     `,2000)
         
     
-    // }}
+    // }
 
-    // add();
-
-    function add1() {
-        setTimeout(()=> document.getElementById("hienthi1").innerHTML += `
-        <div class="m-4">
-            <a href="#">
-                <div class="mb-3 a">
-                    <div style=" width: 100px;">
-                        <img class="rounded-circle"
-                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbKoihkRWHsOFdnwoxoWhkuhb4rC2ViOiy6g&amp;usqp=CAU"
-                            style="width:65%">
-                    </div>
-                    <div class=" text-bottom">
-                        <span class="">
-                            <p style="margin-bottom: 0px;"><b>Nguyên </b></p>
-                        </span>
-
-
-                        <a type="button" id="demo2" onclick="myFFunction2()"
-                            class="btn btn-primary btn-sm d-inline-block"> Thêm bạn bè</a>
-                    </div>
-                </div>
-            </a>
-        </div>
-        `,2000)
-        
-    
-    }
-
-    add1();
+    // add1();
 
 
    
