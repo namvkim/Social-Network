bc32e31996a1d3b07f6a0dcff68b2bb83224e42f
SangDang2308 <sang.dang22@student.passerellesnumeriques.org>
2021-01-28 19:47:37 +0700

    push 28/01 7:47

--- notification.js
+++ notification.js
@@ -15,7 +15,7 @@ var user;
 var pesonal;
 var email = "Lexie.Keeling@yahoo.com";
 var id_user;
-var id_note;
+
 
 
 function get_user() {
@@ -25,8 +25,9 @@ function get_user() {
             if (user[i].email == email) { id_user = parseInt(i) + 1; break; }
         };
         callAPI(`user/${id_user}/notification`, "GET", null).then(res => {
-            notification = res.data;   
-            add2();    
+            notification = res.data;
+            add2();
+            add3();
         });
     });
 }
@@ -36,20 +37,41 @@ get_user();
 
 
 function add2() {
-    for (var i in notification){
-    var n=notification[i].id_notfi-1;
+    for (var i in notification) {
+        var n = notification[i].id_notfi - 1;
         document.getElementById("note1").innerHTML += `
         <a class="dropdown-item" href="#">
             <div class="row">
                 <div class="col-sm-2">
-                    <img src="${user[n].avatar}" alt="" style="height: 40px; width: 40px;">
+                    <img class =" mr-auto" src="${user[n].avatar}" alt="" style="height: 40px; width: 40px; border-radius: 50%; border: 2px solid #a1a1a1">
                 </div>
-                <div class="col-sm-4"><b>${user[n].name}</b>
-                    <p>${user[n].comment}</p>
+                <div class="col-sm-6"><b>${user[n].name}</b>
+                    <p>${notification[i].content}</p>
                 </div>
-                <div class="col-sm-6"><i style="color: cadetblue;">1 giây trước</i></div>
+                <div class="col-sm-4"><i style="color: cadetblue;">1 giây trước</i></div>
             </div>
         </a>
+        `;
+    }
+}
+
+function add3() {
+    for (var i in notification) {
+        var n = notification[i].id_notfi - 1;
+        document.getElementById("note2").innerHTML += `
+            <a class="dropdown-item " href="# ">
+                <div class="row ">
+                    <div class="col-sm-2 ">
+                        <img src="${user[n].avatar}" alt=" " style="height: 40px; width: 40px; border-radius: 50%; border: 2px solid #a1a1a1">
+                    </div>
+                    <div class="col-sm-6"><b>${user[n].name}</b>
+                        <p>Chúc mừng sinh nhật</p>
+                    </div>
+                    <div class="col-sm-4">
+                        <img class="ml-sm-5" src="imgNotificate/birthday.png " alt=" " style="height: 40px; width: 40px; ">
+                    </div>
+                </div>
+            </a>
         `;
     }
 }
--- setting.js
+++ setting.js
@@ -1,5 +1,45 @@
-var infor = {
-    name: "Anna",
-    birthday: "",
-};
+const url_api = "https://600a50de778d1a0017793a0a.mockapi.io/ai";
+var notification = [];
 
+function callAPI(endpoint, method, body) {
+    return axios({
+        method: method,
+        url: `${url_api}/${endpoint}`,
+        data: body,
+    }).catch((err) => {
+        console.log(err);
+    });
+}
+
+var user;
+var pesonal;
+var email = "Lexie.Keeling@yahoo.com";
+var id_user;
+var id_note;
+
+
+function get_user() {
+    callAPI("user", "GET", null).then(res => {
+        user = res.data;
+        for (var i in user) {
+            if (user[i].email == email) { id_user = parseInt(i) + 1; break; }
+        };
+        // callAPI(`user/${id_user}/notification`, "GET", null).then(res => {
+        //     notification = res.data;
+        // });
+    });
+}
+
+
+get_user();
+
+
+function add2() {
+
+    document.getElementById("note2").innerHTML += `
+        <p>${user[i].name}</p>
+        <p></p>
+        <p>Male</p>
+        `;
+
+}
