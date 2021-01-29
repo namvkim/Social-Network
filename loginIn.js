d64c26cdd6a97e766c4c185bad90f235fa21e4cb
hanguyen2911 <nguyenthithuha2911ntth@gmail.com>
2021-01-29 10:32:56 +0700

    update login

--- loginIn.js
+++ loginIn.js
@@ -1,6 +1,6 @@
-
-   const url_api = "https://600a50de778d1a0017793a0a.mockapi.io/ai";
-
+const url_api = "https://600a50de778d1a0017793a0a.mockapi.io/ai";
+ 
+ 
    function callAPI(endpoint, method, body) {
        return axios({
            method: method,
@@ -10,34 +10,69 @@
            console.log(err);
        });
    }
-
+    
    var user;
-   var pesonal;
+    var pesonal;
+    var email = "Turner_Skiles10@hotmail.com";
+    email.toString();
+    var password = parseInt(31032);
+    // var email = document.getElementById('inputEmail');
+    // var password = parseInt(document.getElementById('inputPassword'));
    var id_user;
-   var email = document.getElementById('inputEmail').value;
-   var password = document.getElementById('inputPassword').value;
-        
-
-   function login() {
+   
+   function get_user() {
        callAPI("user", "GET", null).then(res => {
            user = res.data;
-           console.log(user);
            for (var i in user) {
-           if (user[i].email === email) {
-              //if(user[i].password == password) {
-                   return window.location.href = 'http://127.0.0.1:5500/personalPage.html';
-               //}
-           }
-           else {
-               alert("không đúng");
-              window.location.reload();
-               }
-       }
+               if (user[i].email == email) { id_user = parseInt(i) + 1; break; }
+           };
+           callAPI(`user/${id_user}/pesonal`, "GET", null).then(res => {
+               pesonal = res.data;
+               console.log(pesonal);
+               login();
+           });
        });
    }
-
-
-
-
-
-
+get_user()
+   function login() {
+    console.log(user);
+       console.log(email);
+       console.log(password);
+       for (var i in user) {
+           var n = user[i].id - 1;
+           if (user[n].email === email){
+            if( user[n].password == password) {
+              // alert("Đăng nhập thành công")
+                   //  window.location.href = 'http://127.0.0.1:5500/personalPage.html';  
+            }
+            else{
+              //  alert("Đăng nhập không thành công");
+               //window.location.reload();
+            }    
+        }
+       }
+   }
+    
+    
+    
+    
+    
+   function deleteTour(i) {
+       callAPI(`cart/${i}`, "DELETE", null).then(response => {
+           window.location.reload();
+       });
+   }
+    
+    
+    
+    
+      
+    
+    
+    
+    
+    
+    
+    
+    
+     
