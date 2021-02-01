7858d4e746bba60e57c2d4669ce16722a71d98c5
SangDang2308 <sang.dang22@student.passerellesnumeriques.org>
2021-02-01 16:13:50 +0700

    01/02/2021

--- setting.js
+++ setting.js
@@ -15,6 +15,7 @@ var user;
 var pesonal;
 var email = "Lexie.Keeling@yahoo.com";
 var id_user;
+var uers;
 
 
 
@@ -27,6 +28,7 @@ function get_user() {
         add2();
     });
 }
+
 get_user();
 
 function add2() {
@@ -36,16 +38,41 @@ function add2() {
     if (user[id_user - 1].gender == false) {
         gen = "nam";
     } else gen = "nu";
-
     var curDate = new Date(user[id_user - 1].birthday);
     var day = curDate.getDate();
     var month = curDate.getMonth();
     var year = curDate.getFullYear();
     time += day + "/" + month + "/" + year;
-
-
     infor += '<p>' + user[id_user - 1].name + '</p>';
     infor += '<p>' + time + '</p>';
     infor += '<p>' + gen + '</p>';
     document.getElementById("setinfor").innerHTML += infor;
+    console.log(time);
+
 }
+
+function edit() {
+    var gender = "";
+    var birthday;
+    var name = "";
+
+
+}
+
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: -25.344, lng: 131.036 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+    });
+}
+
+initMap();
